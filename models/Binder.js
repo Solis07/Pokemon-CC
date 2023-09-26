96e526d4e5168660a8e7562c31dd238c713367e1
L.J Solis <l.jsolis52@yahoo.com>
2023-09-25 20:40:44 -0500

    Added to the models folder. I also added the code for the auth.js in the utils and for the logout.js in the front end folder. It was created by farjanan. It was to prevent code from being uninterrupted.

---
+++ models/Binder.js
@@ -0,0 +1,35 @@
+const { Model, Datatype } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Binder extends Model { }
+
+Binder.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "binder",
+  }
+);
+
+module.exports = Binder;
--- models/card.js
+++ models/card.js
@@ -1,12 +1,39 @@
-// const { Model, Datatype } = require('sequelize');
-// const sequelize = require('../config/connection');
+const { Model, Datatype } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// class Card extends Model { }
+class Card extends Model { }
 
-// Card.init(
-//   {
+Card.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    series: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "card",
+  }
+);
 
-//   },
-// )
-
-// module.export = Card;
+module.exports = Card;
