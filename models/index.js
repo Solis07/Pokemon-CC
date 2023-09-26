90c4658f4eaeb93eb055d4b9b45fcd50c808c209
Farjana Akhter <farjana.akhter085@gmail.com>
2023-09-25 19:50:53 -0500

    added logout.js file in public folder, auth.js in utils folder

--- models/index.js
+++ models/index.js
@@ -1,8 +1,48 @@
-const User = require('./User');
+// const User = require('./User');
 // const Card = require('./Card');
 
 // Card.belongsTo(User, {
 //   foreignKey: 'user_id'
 // })
 
-module.exports = {User}
+// const User = require('./User');
+// const Card = require('./Post');
+// const Comment = require('./Comment');
+
+// // User have many Post
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+// });
+
+// // Post belongsTo User
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: "cascade"
+// });
+// // Comment belongsTo User
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: "cascade"
+// });
+// // Comment belongsTo Post
+// Comment.belongsTo(Post, {
+//     foreignKey: 'post_id',
+//     onDelete: "cascade"
+// });
+// // User have many Post
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id',
+//     onDelete: "cascade"
+// });
+// // User have many Post
+// Post.hasMany(Comment, {
+//     foreignKey: 'post_id',
+//     onDelete: "cascade"
+// })
+// module.exports = { 
+// User, 
+// Post, 
+// Comment, 
+// };
+
+// module.exports = {User}
