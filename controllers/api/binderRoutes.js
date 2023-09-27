9d774f6a33c2fc2aa62cb51515e80d1407b025ea
L.J Solis <l.jsolis52@yahoo.com>
2023-09-27 18:36:55 -0500

    I fixed the routes and I fixed the relationships in sequilize. The only thing left is to create an add binder and allow the user to add cards and delete cards from their binder.

--- controllers/api/binderRoutes.js
+++ controllers/api/binderRoutes.js
@@ -1,6 +1,26 @@
 const router = require("express").Router();
-const { Binder } = require("../../models");
+const { Binder, Card } = require("../../models");
 const withAuth = require("../../utils/auth");
+
+
+router.post("/", withAuth, (req, res) => {
+  // check the session
+  if (req.session) {
+    Binder.create({
+      id: req.body.id,
+      name: req.body.name,
+      // use the id from the session
+      user_id: req.session.user_id,
+    })
+      .then((dbBinderData) => res.json(dbBinderData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+
 
 router.get("/", (req, res) => {
   Binder.findAll({})
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { Binder, User } = require('../models');
+const { Binder, Card, User } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.render('signup');
+  res.render();
 });
 
 
@@ -15,6 +15,21 @@ router.get("/signup", async (req, res) => {
 });
 
 router.get("/binder", async (req, res) => {
+  const pokemonName = req.query.name;
+  try {
+    const response = await fetch(
+      `https://api.pokemontcg.io/v2/cards?q=name:${pokemonName}`
+    );
+    const data = await response.json();
+    const cards = data.data;
+    res.render("binder", { cards });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching data.");
+  }
+});
+
+router.get("/search", async (req, res) => {
   const pokemonName = req.query.name;
   try {
     const response = await fetch(
